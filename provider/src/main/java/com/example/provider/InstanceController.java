2a4aca9dd09e8d68d121df07b37b9cfb6bc3924a
AMAN <amansrivastav1825@gmail.com>
2025-12-22 02:12:31 +0530

    Interservice-communication using RestTemplate

---
+++ provider/src/main/java/com/example/provider/InstanceController.java
@@ -0,0 +1,20 @@
+package com.example.provider;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InstanceController {
+
+    @Value("${server.port}")
+    private String port;
+
+    private String instanceId = java.util.UUID.randomUUID().toString();
+
+    @GetMapping("/instance-info")
+    public String getInstanceInfo() {
+        System.out.println("Instance received at port " + port);
+        return "Instance served at port " + port + " with instance id " + instanceId;
+    }
+}
